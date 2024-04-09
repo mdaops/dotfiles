e4d77b5e0025651050ebf0b0ab013bdf99d8886a
Michael Abbott <michael@mabbott.dev>
2024-04-09 15:28:15 +0100

    colorscheme -> lsp

--- .config/nvim/lua/base/plugins/colorscheme.lua
+++ .config/nvim/lua/base/plugins/colorscheme.lua
@@ -13,19 +13,19 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = true,
-          mini = {
-            enabled = true,
-            indentscope_color = '',
-          },
-        },
-      }
+      -- require('catppuccin').setup {
+      --   integrations = {
+      --     cmp = true,
+      --     gitsigns = true,
+      --     nvimtree = true,
+      --     treesitter = true,
+      --     notify = true,
+      --     mini = {
+      --       enabled = true,
+      --       indentscope_color = '',
+      --     },
+      --   },
+      -- }
       -- vim.cmd.colorscheme 'catppuccin'
     end,
   },
@@ -40,7 +40,28 @@ return {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
-      require('rose-pine').setup {}
+      require('rose-pine').setup {
+        enable = {
+          terminal = true,
+          legacy_highlights = true,
+          migrations = true,
+        },
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+        highlight_groups = {
+          StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+          StatusLineNC = { fg = 'subtle', bg = 'surface' },
+          TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+          TelescopeNormal = { bg = 'none' },
+          TelescopePromptNormal = { bg = 'base' },
+          TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+          TelescopeSelection = { fg = 'text', bg = 'base' },
+          TelescopeSelectionCaret = { fg = 'rose', bg = 'rose' },
+        },
+      }
       vim.cmd.hi 'Comment gui=none'
       vim.cmd.colorscheme 'rose-pine'
     end,
