e75e3bd51fffa462b56c6ee7c1b05cabfd7551d3
Michael Abbott <michael@mabbott.dev>
2024-04-14 16:53:49 +0100

    bundle elixr lsp with tools

--- .config/nvim/lua/custom/plugins/lang/elixir.lua
+++ .config/nvim/lua/custom/plugins/lang/elixir.lua
@@ -6,19 +6,20 @@ return {
     config = function()
       local elixir = require 'elixir'
       local elixirls = require 'elixir.elixirls'
-
       elixir.setup {
         nextls = { enable = true },
-        credo = {},
+        cmd = { '/home/dev/.config/.elixir-ls/release/language_server.sh' },
         elixirls = {
           enable = true,
           settings = elixirls.settings {
-            dialyzerEnabled = false,
-            enableTestLenses = false,
+            dialyzerEnabled = true,
+            fetchDeps = false,
+            enableTestLenses = true,
+            suggestSpecs = true,
           },
           on_attach = function(client, bufnr)
-            vim.keymap.set('n', '<space>efp', ':ElixirFromPipe<cr>', { buffer = true, noremap = true })
-            vim.keymap.set('n', '<space>etp', ':ElixirToPipe<cr>', { buffer = true, noremap = true })
+            vim.keymap.set('n', '<space>fp', ':ElixirFromPipe<cr>', { buffer = true, noremap = true })
+            vim.keymap.set('n', '<space>tp', ':ElixirToPipe<cr>', { buffer = true, noremap = true })
             vim.keymap.set('v', '<space>em', ':ElixirExpandMacro<cr>', { buffer = true, noremap = true })
           end,
         },
